1a0e4a1593a571cecec98775efd08197f4234785
Freet8707 <freet8707@gmail.com>
2020-07-23 19:19:37 -0500

    reducer function successfully maps through state and can toggle if array objects completed key is true or not

--- todo/src/components/ToDoList.js
+++ todo/src/components/ToDoList.js
@@ -1,21 +1,98 @@
-import React, { useReducer } from 'react'
+import React, { useState, useReducer } from 'react'
+import styled from 'styled-components'
 
 import { reducer } from '../reducers/Reducer'
 
-const initialState = 
-{
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-};
+const BodyDiv = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  min-height: 100vh;
+  margin: 0 auto;
+  background: #d3fffe;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  div {
+    width: 100%;
+    text-align: center;
+    input {
+        width: 80%;
+        margin: 50px 10% 50px 10%;
+        height: 25px;   
+    }
+    button {
+        width: 100%;
+        max-width: 100px;
+        height: 40px;
+    }
+  }
+`;
+
+const NoteCard = styled.span`
+    width: 250px;
+    margin: 50px 25px 0 25px;
+    height: 250px;
+    background: #f8ff49;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;    
+    font-size: 1rem;
+    h2 {
+        font-family: 'Roboto Mono', monospace;
+        font-size: 1.6rem;
+        font-weight: 600;
+        margin: 0;
+        padding: 40px 0 0 20px;
+    }
+    p {
+        padding: 0 0 20px 20px;
+        margin: 0;
+    }
+`;
+
+
 
 const ToDoList = () => {
-    const [state, dispatch] = useReducer(reducer, initialState)
+    const [state, dispatch] = useReducer(
+        reducer, 
+        [
+            {
+                item: 'Learn about reducers',
+                completed: false,
+                id: Math.random()
+            }
+        ]);
+    const [taskName, setTaskName] = useState('')
+
+    const handleChanges = (e) => {
+        setTaskName(e.target.value)
+    }
 
     return(
-        <>
-            {Array.isArray(state) ? <p>ToDoList</p> : <p>{JSON.stringify(initialState)}</p>}
-        </>
+        <BodyDiv>
+            <div>
+                <input 
+                    type='text'
+                    name='taskName'
+                    placeholder='Enter task name'
+                    value={taskName}
+                    onChange={handleChanges}
+                />
+                <button onClick={() => {
+                    dispatch( { type: 'ADD_TODO', payload: taskName})
+                }}>Add Your Task</button>
+            </div>
+            {state.map(item => {
+                return (
+                    <NoteCard key={item.id}>
+                        <h2>Task Name: </h2>
+                        <h2>{item.item}</h2>
+                        <p onClick={() => { dispatch({ type: 'SET_COMPLETE', payload: {complete: !item.completed, id: item.id}})}}>{item.completed ? "Task complete" : "Task incomplete"}</p>
+                    </NoteCard>
+                )
+            })}
+        </BodyDiv>
     )
 }
 
--- todo/src/reducers/Reducer.js
+++ todo/src/reducers/Reducer.js
@@ -1,15 +1,22 @@
-
-
 export const reducer = (state, action) => {
     switch(action.type){
         case 'ADD_TODO':
             return [...state, 
                 {
-                    item: action.payload.item,
-                    completed: action.payload.completed,
-                    id: action.payload.id
+                    item: action.payload,
+                    completed: false,
+                    id: Math.random()
                 }
             ]
+        case 'SET_COMPLETE':
+            console.log(state, action)
+            let newState = state.map(item => {
+                if(item.id === action.payload.id){
+                    return {...item, completed: action.payload.complete};
+                }
+                return item
+            })
+            return state = newState
         default:
             return state
     }
